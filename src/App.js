77f2b529a6add44fe69e20cc233d7ab000920202
Naveen Bellad <belladnaveen027@gmail.com>
2025-12-05 19:55:43 +0530

    Your descriptive message

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         <main className={theme}>
           <Routes>
-            <Route path="/" element={<Hero />} />
+            <Route path="/hero" element={<Hero />} />
             <Route path="/services" element={<Services />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
